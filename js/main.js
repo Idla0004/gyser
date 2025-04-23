64fe2ac91aae19ea4f1b9750832dc49b587fc063
Ida <idla0004@stud.kea.dk>
2025-04-23 15:31:00 +0200

    Ny artikel og nyt farvetema

--- js/main.js
+++ js/main.js
@@ -1,12 +1,54 @@
-document
-  .querySelector("#hoverklovn")
-  .addEventListener("mouseover", headHighLight1);
-document
-  .querySelector("#hoverklovn")
-  .addEventListener("mouseout", headUnHighlight1);
-document.querySelector("#hoverklovn").addEventListener("click", headInfo1);
-document.querySelector("#hoverklovn").addEventListener("click", infoBox1);
-document.querySelector("#hoverklovn").addEventListener("click", infoBox2);
+// #hoverklovn
+const klovn = document.querySelector("#hoverklovn");
+if (klovn) {
+  klovn.addEventListener("mouseover", headHighLight1);
+  klovn.addEventListener("mouseout", headUnHighlight1);
+  klovn.addEventListener("click", headInfo1);
+  klovn.addEventListener("click", infoBox1);
+  klovn.addEventListener("click", infoBox2);
+}
+
+// #hoverkniv
+const kniv = document.querySelector("#hoverkniv");
+if (kniv) {
+  kniv.addEventListener("mouseover", headHighLight2);
+  kniv.addEventListener("mouseout", headUnHighlight2);
+  kniv.addEventListener("click", headInfo2);
+  kniv.addEventListener("click", infoBox3);
+  kniv.addEventListener("click", infoBox4);
+}
+
+// #hovertekst
+const tekst = document.querySelector("#hovertekst");
+if (tekst) {
+  tekst.addEventListener("mouseover", headHighLight3);
+  tekst.addEventListener("mouseout", headUnHighlight3);
+  tekst.addEventListener("click", headInfo3);
+  tekst.addEventListener("click", infoBox5);
+  tekst.addEventListener("click", infoBox6);
+}
+
+// #hovertelefon
+const telefon = document.querySelector("#hovertelefon");
+if (telefon) {
+  telefon.addEventListener("mouseover", headHighLight4);
+  telefon.addEventListener("mouseout", headUnHighlight4);
+  telefon.addEventListener("click", headInfo4);
+  telefon.addEventListener("click", infoBox7);
+  telefon.addEventListener("click", infoBox8);
+}
+
+// #hoverblod
+const blod = document.querySelector("#hoverblod");
+if (blod) {
+  blod.addEventListener("mouseover", headHighLight);
+  blod.addEventListener("mouseout", headUnHighlight);
+  blod.addEventListener("click", headInfo5);
+  blod.addEventListener("click", infoBox9);
+  blod.addEventListener("click", infoBox10);
+}
+
+//funktioner på hoverknapperne
 
 function headHighLight1() {
   console.log("headHighLight1");
@@ -18,34 +60,29 @@ function headUnHighlight1() {
   document.querySelector("#hoverklovn").style.fill = "#fff";
 }
 
+//Trapper
+
 function headInfo1() {
   console.log("headInfo");
   animateBoxes();
 
-  document.querySelector(".info-text > h2").textContent = "klovnen";
+  document.querySelector(".info-text > h2").textContent =
+    "ALDRIG LØB OP AF EN TRAPPE ELLER NED I EN KÆLDER";
   document.querySelector(".info-text > article > p").textContent =
-    "lorem ipsum 40";
+    "Når du er et gyser offer, er trapper og kældre ofte fælder. Undgå at løbe op eller ned ad trapper, da dette ofte fører til at du bliver fanget eller overrasket af en skurk.";
 }
 
 function infoBox1() {
   console.log("headInfo");
-  document.querySelector("#efficiency").textContent = "hahahha";
+  document.querySelector("#efficiency").innerHTML =
+    "<h3>Fare-niveau:</h3> Høj. Trapper og kældre er klassiske steder, hvor ofre bliver fanget i gyserfilm.";
 }
 
 function infoBox2() {
   console.log("headInfo");
-  document.querySelector("#requirement").textContent = "forbliv i live";
+  document.querySelector("#requirement").innerHTML =
+    "<h3>Husk:</h3> Hold dig til åbne områder, hvor du kan se, hvad der sker omkring dig.";
 }
-
-document
-  .querySelector("#hoverkniv")
-  .addEventListener("mouseover", headHighLight2);
-document
-  .querySelector("#hoverkniv")
-  .addEventListener("mouseout", headUnHighlight2);
-document.querySelector("#hoverkniv").addEventListener("click", headInfo2);
-document.querySelector("#hoverkniv").addEventListener("click", infoBox3);
-document.querySelector("#hoverkniv").addEventListener("click", infoBox4);
 
 function headHighLight2() {
   console.log("headHighLight2");
@@ -57,34 +94,28 @@ function headUnHighlight2() {
   document.querySelector("#hoverkniv").style.fill = "#fff";
 }
 
+// Telefon
+
 function headInfo2() {
   console.log("headInfo");
   animateBoxes();
 
-  document.querySelector(".info-text > h2").textContent = "Kniven";
+  document.querySelector(".info-text > h2").textContent = "IKKE TAG TELEFONEN!";
   document.querySelector(".info-text > article > p").textContent =
-    "lorem ipsum 30";
+    "Telefoner er ofte ustabile i gyserfilm og kan være en kilde til forstyrrelser eller sporing. Undgå at tage telefonen, da det kan afsløre din position eller give dig falske sikkerhedsfornemmelser.";
 }
 
 function infoBox3() {
   console.log("headInfo");
-  document.querySelector("#efficiency").textContent = "fyld tekst";
+  document.querySelector("#efficiency").innerHTML =
+    "<h3>Fare-niveau:</h3> Middel. Telefoner kan være nyttige, men de kan også være risikable, hvis de bliver sporet.";
 }
 
 function infoBox4() {
   console.log("headInfo");
-  document.querySelector("#requirement").textContent = "mere tekst";
+  document.querySelector("#requirement").innerHTML =
+    "<h3>Husk:</h3> Brug en gammeldags radio eller walkie-talkie for at kommunikere, hvis nødvendigt.";
 }
-
-document
-  .querySelector("#hovertekst")
-  .addEventListener("mouseover", headHighLight3);
-document
-  .querySelector("#hovertekst")
-  .addEventListener("mouseout", headUnHighlight3);
-document.querySelector("#hovertekst").addEventListener("click", headInfo3);
-document.querySelector("#hovertekst").addEventListener("click", infoBox5);
-document.querySelector("#hovertekst").addEventListener("click", infoBox6);
 
 function headHighLight3() {
   console.log("headHighLight3");
@@ -96,34 +127,30 @@ function headUnHighlight3() {
   document.querySelector("#hovertekst").style.fill = "#fff";
 }
 
+//Huse
+
 function headInfo3() {
   console.log("headInfo");
   animateBoxes();
-  document.querySelector(".info-text > h2").textContent = "teksten";
+  document.querySelector(".info-text > h2").textContent =
+    "HOLD DIG FRA GAMLE, MÅSKE HJEMSØGTE, HUSE!";
   document.querySelector(".info-text > article > p").textContent =
-    "lorem ipsum 30";
+    "Gamle, hjemsøgte huse er aldrig et godt sted at være i et gyserfilms univers. Undgå at gå ind i sådanne steder, da de ofte er fyldt med fælder og overnaturlige trusler.";
 }
 
 function infoBox5() {
   console.log("headInfo");
-  document.querySelector("#efficiency").textContent = "fyld teksten ud";
+  document.querySelector("#efficiency").innerHTML =
+    "<h3>Fare-niveau:</h3> Meget høj. Hjemsøgte huse er klassiske scenarier for gyserfilm, hvor ofre ofte mister livet.";
 }
 
 function infoBox6() {
   console.log("headInfo");
-  document.querySelector("#requirement").textContent =
-    "mere tekst mere teeeekst";
+  document.querySelector("#requirement").innerHTML =
+    "<h3>Husk:</h3> Find et sikkert, velbelyst sted at opholde dig, fremfor at undersøge mystiske bygninger.";
 }
 
-document
-  .querySelector("#hovertelefon")
-  .addEventListener("mouseover", headHighLight4);
-document
-  .querySelector("#hovertelefon")
-  .addEventListener("mouseout", headUnHighlight4);
-document.querySelector("#hovertelefon").addEventListener("click", headInfo4);
-document.querySelector("#hovertelefon").addEventListener("click", infoBox7);
-document.querySelector("#hovertelefon").addEventListener("click", infoBox8);
+//Bagsædet
 
 function headHighLight4() {
   console.log("headHighLight");
@@ -138,31 +165,23 @@ function headUnHighlight4() {
 function headInfo4() {
   console.log("headInfo");
   animateBoxes();
-  document.querySelector(".info-text > h2").textContent = "blodet";
+  document.querySelector(".info-text > h2").textContent =
+    "ALTID TJEK BAGSÆDET I DIN BIL!";
   document.querySelector(".info-text > article > p").textContent =
-    "lorem ipsum 30";
+    "Før du sætter dig ind i din bil og kører afsted, tjek altid bagsædet for at sikre dig, at der ikke er nogen skjult der. Dette kan forhindre overraskelser på vejen.";
 }
 
 function infoBox7() {
   console.log("headInfo");
-  document.querySelector("#efficiency").textContent = "osvosv";
+  document.querySelector("#efficiency").innerHTML =
+    "<h3>Fare-niveau:</h3> Høj. Bagsæder er ofte steder, hvor skurke gemmer sig i gyserfilm.";
 }
 
 function infoBox8() {
   console.log("headInfo");
-  document.querySelector("#requirement").textContent =
-    "mere tekst mere teeeekst";
+  document.querySelector("#requirement").innerHTML =
+    "<h3>Husk:</h3> Lås altid dine bilporte, når du ikke er i bilen, for at forhindre uønsket adgang.";
 }
-
-document
-  .querySelector("#hoverblod")
-  .addEventListener("mouseover", headHighLight);
-document
-  .querySelector("#hoverblod")
-  .addEventListener("mouseout", headUnHighlight);
-document.querySelector("#hoverblod").addEventListener("click", headInfo5);
-document.querySelector("#hoverblod").addEventListener("click", infoBox9);
-document.querySelector("#hoverblod").addEventListener("click", infoBox10);
 
 function headHighLight() {
   console.log("headHighLight");
@@ -177,20 +196,22 @@ function headUnHighlight() {
 function headInfo5() {
   console.log("headInfo");
   animateBoxes();
-  document.querySelector(".info-text > h2").textContent = "Blodet";
+  document.querySelector(".info-text > h2").textContent =
+    "ALDRIG SIG 'JEG KOMMER STRAKS TILBAGE'";
   document.querySelector(".info-text > article > p").textContent =
-    "lorem ipsum 30";
+    "At sige 'Jeg kommer straks tilbage' er et klassisk ord for de tabte karakterer i gyserfilm. Undgå at sige dette, da det ofte signalerer, at noget skal gå galt.";
 }
 
 function infoBox9() {
   console.log("headInfo");
-  document.querySelector("#efficiency").textContent = "osvosv";
+  document.querySelector("#efficiency").innerHTML =
+    "<h3>Fare-niveau:</h3> Middel. Selvom det ikke direkte forårsager fare, kan det være et tegn på, at du er for selvsikker.";
 }
 
 function infoBox10() {
   console.log("headInfo");
-  document.querySelector("#requirement").textContent =
-    "mere tekst mere teeeekst";
+  document.querySelector("#requirement").innerHTML =
+    "<h3>Husk:</h3> Hold din plan for dig selv eller del den kun med personer, du stoler på fuldt ud.";
 }
 
 function animateBoxes() {
@@ -213,15 +234,60 @@ function cleanup() {
   document.querySelector("#requirement").classList.remove("fadeIn");
 }
 
-const openBtn = document.querySelector("#openBtn");
-openBtn.addEventListener("click", openDialog);
-function openDialog(e) {
-  e.preventDefault();
-  document.querySelector("#dialog-1").showModal();
+//panorama
+
+const pan = document.querySelector("#pan");
+
+if (pan) {
+  // Tilføj animation én gang
+  pan.classList.add("pan_animation");
+
+  // Start animation ved hover
+  pan.addEventListener("mouseover", () => {
+    pan.classList.add("running");
+  });
+
+  // Stop animation når musen fjernes
+  pan.addEventListener("mouseout", () => {
+    pan.classList.remove("running");
+  });
 }
 
-const closeBtn = document.querySelector("#closeBtn");
-closeBtn.addEventListener("click", closeDialog);
-function closeDialog() {
-  document.querySelector("#dialog-1").close();
+// //Dialog buttons
+
+function setupArticle(article) {
+  const openBtn = article.querySelector(".openBtn");
+  const dialog = article.querySelector("dialog");
+  const closeBtn = dialog.querySelector("#closeBtn");
+
+  openBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    dialog.showModal();
+  });
+
+  closeBtn.addEventListener("click", function () {
+    dialog.close();
+  });
+}
+
+const articles = document.querySelectorAll(".news_card");
+
+articles.forEach(setupArticle);
+
+// DARK MODE //
+
+const css = document.querySelector("#lightmode");
+
+const btn3 = document.querySelector("#changeBtn3");
+btn3.addEventListener("click", changeMode3);
+const switchtext3 = document.querySelector("#switchtext1");
+
+function changeMode3() {
+  if (css.href.includes("css/dark.css")) {
+    css.href = "css/custom.css";
+    switchtext3.textContent = "Dark Mode";
+  } else {
+    css.href = "css/dark.css";
+    switchtext3.textContent = "Light Mode";
+  }
 }
